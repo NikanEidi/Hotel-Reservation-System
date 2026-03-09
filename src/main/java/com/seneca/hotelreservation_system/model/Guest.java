4e00d96c0e0ddc56a9934b21acca26b0082a9f93
NikanEidi <nikaneydi1984@gmail.com>
2026-03-09 18:56:27 -0400

    [?25hGuest Class with all Variables added!

---
+++ src/main/java/com/seneca/hotelreservation_system/model/Guest.java
@@ -0,0 +1,63 @@
+package com.seneca.hotelreservation_system.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "guest")
+public class Guest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "guest_id")
+    private Long guestId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phone;
+
+    @Column(name = "loyalty_points")
+    private int loyaltyPoints;
+
+    @OneToMany(mappedBy = "guest", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Reservation> reservations;
+
+    public Guest() {}
+
+    public Guest(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.loyaltyPoints = 0;
+    }
+
+    public Long getGuestId() { return guestId; }
+    public void setGuestId(Long guestId) { this.guestId = guestId; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+
+    public int getLoyaltyPoints() { return loyaltyPoints; }
+    public void setLoyaltyPoints(int loyaltyPoints) { this.loyaltyPoints = loyaltyPoints; }
+
+    public List<Reservation> getReservations() { return reservations; }
+    public void setReservations(List<Reservation> reservations) { this.reservations = reservations; }
+}
